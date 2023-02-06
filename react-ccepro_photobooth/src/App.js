d5f40c5231194c140e3241b7c335ae1a5997768d
TheRhettThompson <rthompsoncareer@gmail.com>
2023-02-06 19:04:20 +0000

    imported views

--- react-ccepro_photobooth/src/App.js
+++ react-ccepro_photobooth/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
+import Landing from './views/landing';
 import Navbar from './components/navbar';
 import Footer from './components/footer';
 import AboutUs from './views/aboutUs';
+import ClientLogin from './views/clientlogin';
+import Modal from './views/modal';
+import ContactUs from './views/contactus';
+import EventPlanner from './views/eventplanner';
+import Gallery from './views/gallery';
+import Home from './views/home';
 import logo from './logo.svg';
 
 
@@ -15,7 +22,6 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Landing />} />
-          <Route exact path="/landing" element={<Landing />} />
           <Route exact path="/modal" element={<Modal />} />
           <Route exact path="/aboutus" element={<AboutUs />} />
           <Route exact path="/clientlogin" element={<ClientLogin />} />
